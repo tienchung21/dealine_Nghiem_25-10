c39019254987ec4034736c963948d9965568477c
tienchung21 <chung9atm@gmail.com>
2024-10-16 19:21:16 +0700

    abc

---
+++ project1/style/style.js
@@ -0,0 +1,65 @@
+
+    function on() {
+        document.getElementById("new-modal").style.display = "flex";
+      }
+      
+      function off() {
+        document.getElementById("new-modal").style.display = "none";
+      }
+    
+      function on2() {
+        document.getElementById("new-modal2").style.display = "flex";
+      }
+      
+      function off2() {
+        document.getElementById("new-modal2").style.display = "none";
+      }
+      function on3() {
+        document.getElementById("new-modal3").style.display = "flex";
+      }
+      
+      function off3() {
+        document.getElementById("new-modal3").style.display = "none";
+      }
+      function foron1() {
+        document.getElementById("fortfolio-modal1").style.display = "flex";
+      }
+      
+      function oroff1() {
+        document.getElementById("fortfolio-modal1").style.display = "none";
+      }
+
+      function foron2() {
+        document.getElementById("fortfolio-modal2").style.display = "flex";
+      }
+      
+      function oroff2() {
+        document.getElementById("fortfolio-modal2").style.display = "none";
+      }
+      function foron3() {
+        document.getElementById("fortfolio-modal3").style.display = "flex";
+      }
+      
+      function oroff3() {
+        document.getElementById("fortfolio-modal3").style.display = "none";
+      }
+      
+
+      
+
+      function foron4() {
+        document.getElementById("fortfolio-modal4").style.display = "flex";
+      }
+      
+      function oroff4() {
+        document.getElementById("fortfolio-modal4").style.display = "none";
+      }
+      function foron5() {
+        document.getElementById("fortfolio-modal5").style.display = "flex";
+      }
+      
+      function oroff5() {
+        document.getElementById("fortfolio-modal5").style.display = "none";
+      }
+
+
